c31d740cefbc7a9a5a2c03227ce9920dfab10482
filippakarlsson <kafi24gt@student.ju.se>
2025-03-11 13:33:37 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -101,7 +101,7 @@ const products = {
   },
   304: {
     name: "Bold Black heels ",
-    image: "images/heels4.jpg",
+    image: "images/heels4.jpeg",
     price: "$15.00",
     description: "Be bold, be brave, wear these stylish black heels",
   },
